3623f00026dc10c55e4bdb2500ffe7fe29557380
Brent Hagany <bhagany@hiidef.com>
2010-11-09 13:54:08 -0600

    Update CassandraCachingServer to use redis, because PageGetter requires it now

--- hiispider/servers/cassandracaching.py
+++ hiispider/servers/cassandracaching.py
@@ -1,26 +1,22 @@
-from datetime import datetime
-import urllib
-from uuid import uuid4
-from twisted.internet.defer import DeferredList
-from twisted.internet import reactor
+import txredisapi
 from telephus.protocol import ManagedCassandraClientFactory
 from telephus.client import CassandraClient
-from telephus.cassandra.ttypes import ColumnPath, ColumnParent, Column
-from .base import BaseServer, LOGGER
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+from .base import BaseServer
 from ..pagegetter import PageGetter
-from ..exceptions import DeleteReservationException
 
 class CassandraCachingServer(BaseServer):
-    
     def __init__(self,
                  aws_access_key_id=None,
                  aws_secret_access_key=None,
                  cassandra_server=None,
                  cassandra_port=9160,
-                 cassandra_keyspace=None, 
+                 cassandra_keyspace=None,
                  cassandra_cf_cache=None,
                  cassandra_http=None,
                  cassandra_headers=None,
+                 redis_hosts=None,
                  max_simultaneous_requests=100,
                  max_requests_per_host_per_second=0,
                  max_simultaneous_requests_per_host=0,
@@ -46,11 +42,15 @@ class CassandraCachingServer(BaseServer):
         self.cassandra_headers=cassandra_headers
         self.cassandra_factory = ManagedCassandraClientFactory()
         self.cassandra_client = CassandraClient(self.cassandra_factory, cassandra_keyspace)
+        self.setup_redis_client_and_pg(redis_hosts)
         reactor.connectTCP(cassandra_server, cassandra_port, self.cassandra_factory)
-        self.pg = PageGetter(
-            self.cassandra_client, 
-            self.cassandra_cf_cache,
-            self.cassandra_http,
-            self.cassandra_headers,
-            rq=self.rq)
-        
+
+    @inlineCallbacks
+    def setup_redis_client_and_pg(self, redis_hosts):
+        self.redis_client = yield txredisapi.RedisShardingConnection(redis_hosts)
+        self.pg = PageGetter(self.cassandra_client,
+                             self.cassandra_cf_cache,
+                             self.cassandra_http,
+                             self.cassandra_headers,
+                             redis_client=self.redis_client,
+                             rq=self.rq)
