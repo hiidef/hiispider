ca80d33c82bf39a1c44a5e05e434511f1eeefd17
Jason Moiron <jmoiron@jmoiron.net>
2011-06-20 23:08:22 -0400

    add test suite to setuptools package, exclude tests/ez_setup/etc, nose-ify test suite (nosetests now works)

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     name = "HiiSpider",
 
     version = "0.9.8",
-
-    packages = find_packages(),
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
 
     install_requires = ['twisted>=8.1', 'genshi>=0.5.1', 'python-dateutil>=1.4', 'simplejson>=2.1.3', 'boto'],
     include_package_data = True,
@@ -17,6 +16,7 @@ setup(
     description = "HiiDef Web Services web crawler",
     license = "MIT License",
     keywords = "twisted spider crawler",
-    url = "http://github.com/rays/hiispider"
+    url = "http://github.com/hiidef/hiispider",
+    test_suite="tests",
 
 )
