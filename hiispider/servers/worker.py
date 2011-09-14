0afe501c49ee93f150d731a552343d121961cd23
John Wehr <johnwehr@gmail.com>
2011-09-14 19:22:37 -0400

    Upped active jobs to chunk size * 4.

--- hiispider/servers/worker.py
+++ hiispider/servers/worker.py
@@ -32,7 +32,7 @@ class WorkerServer(CassandraServer, JobQueueMixin, PageCacheQueueMixin, JobGette
         self.setupPageCacheQueue(config)
         self.setupJobGetter(config)
         self.request_chunk_size = config.get("amqp_prefetch_count", 10) * 4
-        self.jobs_semaphore = DeferredSemaphore(self.request_chunk_size * 2)
+        self.jobs_semaphore = DeferredSemaphore(config.get("amqp_prefetch_count", 10) * 2)
         # HTTP interface
         resource = WorkerResource(self)
         if port is None:
