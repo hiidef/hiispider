d2eb0c7fb1c18c5a6121cc1d200299e9f262da30
John Wehr <johnwehr@gmail.com>
2011-09-21 19:47:28 -0400

    Added batch functionality.

--- hiispider/servers/mixins/jobgetter.py
+++ hiispider/servers/mixins/jobgetter.py
@@ -15,7 +15,7 @@ class JobGetterBatchException(Exception):
 class JobGetterMixin(MySQLMixin):
 
     request_queue = {}
-    batch_size = 100
+    batch_size = 10
 
     def setupJobGetter(self, config):
         self.setupMySQL(config)
