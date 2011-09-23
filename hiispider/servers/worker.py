5afc44d925bf71c2e9bae9507ab4024688077209
John Wehr <johnwehr@gmail.com>
2011-09-23 15:00:08 -0400

    Callbacks.

--- hiispider/servers/worker.py
+++ hiispider/servers/worker.py
@@ -24,7 +24,7 @@ class WorkerServer(CassandraServer, JobQueueMixin, PageCacheQueueMixin, JobGette
     public_ip = None
     local_ip = None
     network_information = {}
-    simultaneous_reqs = 30
+    simultaneous_reqs = 20
     uuids_dequeued = 0
     jobs_complete = 0
     job_failures = 0
