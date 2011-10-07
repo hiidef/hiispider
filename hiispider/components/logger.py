988d880752fcbd268aaa60515a53a4e2e80c9f90
Jason Moiron <jmoiron@jmoiron.net>
2011-10-07 18:50:06 -0400

    add logger class, still experimental

--- hiispider/components/logger.py
+++ hiispider/components/logger.py
@@ -1,3 +1,5 @@
+
+
 from .base import Component, shared
 from twisted.internet.defer import inlineCallbacks, Deferred
 from copy import copy
@@ -5,14 +7,43 @@ from twisted.internet import task
 import time
 import logging
 
+from pylogd.handlers import PylogdHandler
+from pylogd.twisted import socket
 
-LOGGER = logging.getLogger(__name__)
 
+logger = logging.getLogger(__name__)
 
-class Logger(Component):
+class Logger(Component, PylogdHandler):
 
     def __init__(self, server, config, address=None, **kwargs):
         super(Logger, self).__init__(server, address=address)
         config = copy(config)
         config.update(kwargs)
+        try:
+            self.path = config['logd_path']
+        except KeyError:
+            raise Exception("Logger component requires configuration option `logd_path`.")
+        self.logd_port = config.get('logd_port', 8126)
+        self.logd_host = config.get('logd_host', 'localhost')
+        self.logger = config.get('base_logger', logging.getLogger())
+        self.sock = None
+        logging.Handler.__init__(self)
+
+    def initialize(self):
+        if self.server_mode:
+            self.sock = socket.UDPSocket(self.logd_host, self.logd_port)
+            if self.__class__ not in (x.__class__ for x in self.logger.handlers):
+                self.logger.addHandler(self)
+            self.initialized = True
+
+    # make sure this stuff isn't done
+    def makeSocket(self): return None
+    def createSocket(self): return None
+    def closeOnError(self):
+        import ipdb; ipdb.set_trace();
+
+
+    @shared
+    def send(self, s):
+        self.sock.sendto(s, (self.logd_host, self.logd_port))
 
