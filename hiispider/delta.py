225c575dd9775de539488aa936196ca315daa2c1
Ray Slakinski <ray.slakinski@gmail.com>
2011-11-15 13:45:25 -0500

    more debugging

--- hiispider/delta.py
+++ hiispider/delta.py
@@ -147,14 +147,14 @@ def _narrow(a, b, path):
     Recursively remove keys not in path. Combine lists of lists if
     indicated by the path.
     """
-    if not b:
-        # FIXME: Debug Set Trace
-        import ipdb
-        ipdb.set_trace()
-        if isinstance(a, dict):
-            b = {}
-        elif isinstance(a, list):
-            b = []
+    # if not b:
+    #     # FIXME: Debug Set Trace
+    #     import ipdb
+    #     ipdb.set_trace()
+    #     if isinstance(a, dict):
+    #         b = {}
+    #     elif isinstance(a, list):
+    #         b = []
     # If the path is empty, no need to narrow any further.
     # If there is nothing to narrow, no need to narrow further.
     if not path or (not a and not b):
